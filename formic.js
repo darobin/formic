e20bd0627ac0e58f2ae436b2134b0ac21198ebf0
Robin Berjon <robin@berjon.com>
2014-02-17 17:23:10 +0100

    dump of current state, will all change anyway

--- formic.js
+++ formic.js
@@ -1,32 +1,79 @@
+/*global Element*/
 
 (function (global) {
-    var Formic = function (form) {
-        this.form = form;
-    };
-    Formic.prototype = {
-        // return a list of submittable elements for this form
-        // this isn't live, we'll do mutations later
-        submittableElements: function () {
-            
+    // inject formic
+    global.formic = function () {
+        // a useful addition to elements in general
+        Object.defineProperty(Element.prototype, "ancestors", {
+            enumerable: true
+        ,   get:    function () {
+                var ret = [], el = this;
+                while (el.parentNode.nodeType === Node.ELEMENT_NODE) {
+                    ret.push(el.parentNode);
+                    el = el.parentNode;
+                }
+                return ret;
+            }
+        });
+        
+        // make matches() work
+        if (!Element.prototype.matches) {
+            Element.prototype.matches = function (sels, refs) {
+                return (this.mozMatchesSelector ||
+                        this.msMatchesSelector ||
+                        this.webkitMatchesSelector).call(this, sels, refs);
+            };
         }
-        // construct and return the data set for this form
-        // not live at this point, we'll set up a version that decorates the form later
-    ,   formDataSet:    function () {
-            
-        }
-        // returns the proper (JSON+files) data structure for this form
-    ,   toData: function () {
-            
-        }
-        // encodes for submission
-        // that's simple JSON if there are no files, JSON+multipart otherwise
-    ,   encode: function () {
-            
-        }
-    };
-    
-    
-    global.formic = function (form) {
-        return new Formic(form);
+
+        // make form elements list their submittable elements
+        Object.defineProperty(HTMLFormElement.prototype, "submittableElements", {
+            enumerable: true
+        ,   get:    function () {
+                return this.ownerDocument
+                           .querySelectorAll("button, input, keygen, object, select, textarea")
+                           .filter(function (el) {
+                               return el.form === this;
+                           });
+            }
+        });
+
+        // return the form's data set
+        // XXX we don't have a notion of "submitter" for buttons; we should
+        Object.defineProperty(HTMLFormElement.prototype, "formDataSet", {
+            enumerable: true
+        ,   get:    function () {
+                var controls = this.submittableElements
+                ,   dataSet = []
+                ;
+                for (var i = 0, n = controls.length; i < n; i++) {
+                    var field = controls[i];
+                    // skip element
+                    // we don't check that objects must have loaded a plugin. Bite me.
+                    if (field.ancestors.filter(function (el) { el.tagName.toLowerCase() === "datalist"; }).length ||
+                        field.disabled ||
+                        (
+                            field.matches("button[type='submit'], input[type='submit'], input[type='image'], input[type='button']") &&
+                            field !== this.submitter
+                        ) ||
+                        (field.type === "checkbox" && !field.checked) ||
+                        (field.type === "radio" && !field.checked) ||
+                        (field.type !== "image" && !field.name)
+                    ) continue;
+                    if (field.type === "image") {
+                        var name = field.name ? field.name + "." : ""
+                        ,   nameX = name + "x"
+                        ,   nameY = name + "y"
+                        ;
+                        if (field === this.submitter) {
+                            // XXX we don't have that information
+                            dataSet.push({ name: nameX, value: "0", type: "image" });
+                            dataSet.push({ name: nameY, value: "0", type: "image" });
+                            continue;
+                        }
+                    }
+                }
+                return dataSet;
+            }
+        });
     };
 }(window));
