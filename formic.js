c72e99a30de24686dc5bf1b7c8639fe3ccb03d25
Robin Berjon <robin@berjon.com>
2014-02-18 15:56:26 +0100

    make formic more usable as a helper library

--- formic.js
+++ formic.js
@@ -1,79 +1,60 @@
-/*global Element*/
 
-(function (global) {
-    // inject formic
-    global.formic = function () {
-        // a useful addition to elements in general
-        Object.defineProperty(Element.prototype, "ancestors", {
-            enumerable: true
-        ,   get:    function () {
-                var ret = [], el = this;
-                while (el.parentNode.nodeType === Node.ELEMENT_NODE) {
-                    ret.push(el.parentNode);
-                    el = el.parentNode;
-                }
-                return ret;
-            }
-        });
-        
-        // make matches() work
-        if (!Element.prototype.matches) {
-            Element.prototype.matches = function (sels, refs) {
-                return (this.mozMatchesSelector ||
-                        this.msMatchesSelector ||
-                        this.webkitMatchesSelector).call(this, sels, refs);
-            };
+// this is largely a set of facilities to help with the manipulating of forms
+// ideally, some of this would be made part of the platform
+
+(function () {
+    var filter = Array.prototype.filter
+    // ,   forEach = Array.prototype.forEach
+    ;
+    var formic = {
+        matches:    function (el, sels, refs) {
+            return (
+                    el.matches ||
+                    el.mozMatchesSelector ||
+                    el.webkitMatchesSelector ||
+                    el.msMatchesSelector
+                    ).call(el, sels, refs);
         }
-
-        // make form elements list their submittable elements
-        Object.defineProperty(HTMLFormElement.prototype, "submittableElements", {
-            enumerable: true
-        ,   get:    function () {
-                return this.ownerDocument
-                           .querySelectorAll("button, input, keygen, object, select, textarea")
-                           .filter(function (el) {
-                               return el.form === this;
-                           });
+    ,   ancestors:  function (el) {
+            var ret = [];
+            while (el.parentNode.nodeType === Node.ELEMENT_NODE) {
+                ret.push(el.parentNode);
+                el = el.parentNode;
             }
-        });
+            return ret;
+        }
+    ,   submittableElements:    function (form) {
+            return filter.call( document.querySelectorAll("button, input, keygen, object, select, textarea")
+                            ,   function (el) {
+                                    return form === el.form;
+                                }
+                            );
+        }
+    ,   disabled:   function (el) {
+            return formic.matches(el, ":disabled");
+        }
+    ,   formDataSet:    function (form) {
+            var controls = formic.submittableElements(form)
+            ,   dataSet = []
+            ;
+            for (var i = 0, n = controls.length; i < n; i++) {
+                var field = controls[i];
+                // Fields To Skip
+                // We don't check that objects must have loaded a plugin. Bite me.
+                // Since this is batched, we also don't know the submitter and therefore ignore them all
+                if (formic.ancestors(field).filter(function (el) { el.tagName.toLowerCase() === "datalist"; }).length) continue;
+                if (formic.disabled(field)) continue;
+                if (field.type === "submit" || field.type === "image" || field.type === "button") continue;
+                if (field.type === "checkbox" && !field.checked) continue;
+                if (field.type === "radio" && !field.checked) continue;
+                if (field.type !== "image" && !field.name) continue;
 
-        // return the form's data set
-        // XXX we don't have a notion of "submitter" for buttons; we should
-        Object.defineProperty(HTMLFormElement.prototype, "formDataSet", {
-            enumerable: true
-        ,   get:    function () {
-                var controls = this.submittableElements
-                ,   dataSet = []
-                ;
-                for (var i = 0, n = controls.length; i < n; i++) {
-                    var field = controls[i];
-                    // skip element
-                    // we don't check that objects must have loaded a plugin. Bite me.
-                    if (field.ancestors.filter(function (el) { el.tagName.toLowerCase() === "datalist"; }).length ||
-                        field.disabled ||
-                        (
-                            field.matches("button[type='submit'], input[type='submit'], input[type='image'], input[type='button']") &&
-                            field !== this.submitter
-                        ) ||
-                        (field.type === "checkbox" && !field.checked) ||
-                        (field.type === "radio" && !field.checked) ||
-                        (field.type !== "image" && !field.name)
-                    ) continue;
-                    if (field.type === "image") {
-                        var name = field.name ? field.name + "." : ""
-                        ,   nameX = name + "x"
-                        ,   nameY = name + "y"
-                        ;
-                        if (field === this.submitter) {
-                            // XXX we don't have that information
-                            dataSet.push({ name: nameX, value: "0", type: "image" });
-                            dataSet.push({ name: nameY, value: "0", type: "image" });
-                            continue;
-                        }
-                    }
-                }
-                return dataSet;
+                // XXX take files into account
+
             }
-        });
+            return dataSet;
+        }
     };
-}(window));
+    
+    window.formic = formic;
+}());
